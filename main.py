31a9d6c0e2b27b4e8f911750eed3ebc77772e8ad
jafaripour2001-cell <jafaripour2001@gmail.com>
2026-09-25 23:13:45 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,102 +1,63 @@
-import requests
-import sqlite3
-
-API_URL = "https://jsonplaceholder.typicode.com/users"
-DB_NAME = "users.db"
-
-
-def get_users():
-    try:
-        response = requests.get(API_URL, timeout=5)
-        response.raise_for_status()
-        return response.json()
-
-    except requests.exceptions.RequestException as e:
-        print("خطا در اتصال به API:", e)
-        return None
+from api import get_users
+from database import (
+    create_database,
+    database_is_empty,
+    save_users,
+    show_users,
+    update_user,
+    delete_user
+)
 
 
-def create_database():
-    connection = sqlite3.connect(DB_NAME)
-    cursor = connection.cursor()
+def database_menu():
+    while True:
+        print("\n===== مدیریت دیتابیس =====")
+        print("1. نمایش کاربران")
+        print("2. تغییر ایمیل کاربر")
+        print("3. حذف کاربر")
+        print("4. خروج")
 
-    cursor.execute("DROP TABLE IF EXISTS users")
+        choice = input("گزینه مورد نظر را وارد کنید: ")
 
-    cursor.execute("""
-        CREATE TABLE users (
-            id INTEGER PRIMARY KEY,
-            name TEXT,
-            username TEXT,
-            email TEXT,
-            city TEXT
-        )
-    """)
+        if choice == "1":
+            show_users()
 
-    connection.commit()
-    connection.close()
+        elif choice == "2":
+            update_user()
 
+        elif choice == "3":
+            delete_user()
 
-def save_users(users):
-    connection = sqlite3.connect(DB_NAME)
-    cursor = connection.cursor()
+        elif choice == "4":
+            print("برنامه به پایان رسید.")
+            break
 
-    for user in users:
-        cursor.execute("""
-            INSERT OR REPLACE INTO users
-            (id, name, username, email, city)
-            VALUES (?, ?, ?, ?, ?)
-        """, (
-            user["id"],
-            user["name"],
-            user["username"],
-            user["email"],
-            user["address"]["city"]
-        ))
-
-    connection.commit()
-    connection.close()
-
-
-def show_users():
-    connection = sqlite3.connect(DB_NAME)
-    cursor = connection.cursor()
-
-    cursor.execute("""
-        SELECT id, name, username, email, city
-        FROM users
-    """)
-
-    users = cursor.fetchall()
-
-    connection.close()
-
-    print("\nفهرست کاربران ذخیره شده در SQLite:")
-
-    for user in users:
-        print("--------------------")
-        print("ID:", user[0])
-        print("نام:", user[1])
-        print("نام کاربری:", user[2])
-        print("ایمیل:", user[3])
-        print("شهر:", user[4])
+        else:
+            print("گزینه نامعتبر است.")
 
 
 def main():
     print("برنامه شروع شد")
 
-    users = get_users()
-
-    if users is None:
-        print("دریافت اطلاعات انجام نشد.")
-        return
-
-    print("تعداد کاربران دریافت شده:", len(users))
-
     create_database()
-    save_users(users)
-    show_users()
 
-    print("اطلاعات با موفقیت در SQLite ذخیره شد.")
+    if database_is_empty():
+        print("دیتابیس خالی است؛ دریافت اطلاعات از API...")
+
+        users = get_users()
+
+        if users is None:
+            print("دریافت اطلاعات انجام نشد.")
+            return
+
+        print("تعداد کاربران دریافت شده:", len(users))
+
+        save_users(users)
+
+    else:
+        print("اطلاعات از قبل در دیتابیس وجود دارد.")
+
+    database_menu()
 
 
 if __name__ == "__main__":
